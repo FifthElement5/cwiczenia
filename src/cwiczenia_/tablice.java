0cb19f2c1962af871ee48a0d7bae58eeb13485ec
FifthElement <i.m.galazewska@gmail.com>
2024-11-19 18:36:54 +0100

    first

---
+++ src/cwiczenia_/tablice.java
@@ -0,0 +1,16 @@
+package cwiczenia_;
+
+public class tablice {
+	public static void  main (String[] args) {
+		
+		int[] Array1d = {1, 2, 3, 4, 5, 6};
+		
+		int[][] Array2d = new int[2][3];
+		
+		void drukuj(int []x) {
+		for (int i = 0; i< x.length; i++) {
+			System.out.print(x.[i]);
+			}
+		}
+	}
+}
