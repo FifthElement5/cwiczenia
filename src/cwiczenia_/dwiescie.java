9bf7aba997b40d3395b4dcbe8a028a41539627b0
FifthElement <i.m.galazewska@gmail.com>
2024-11-21 09:26:40 +0100

    2

---
+++ src/cwiczenia_/dwiescie.java
@@ -0,0 +1,40 @@
+
+package cwiczenia_;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class dwiescie {
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 Random random = new Random();
+		 int randomNumber = random.nextInt(200);
+		 
+		  System.out.println("wylosowamo: " + randomNumber);
+		 int wynik = 0; 
+		 while(wynik == 0) {
+		 
+		 System.out.println("zgadnij liczbe od 1 do 200: ");
+		 int liczba = scanner.nextInt();
+		 
+		 if (liczba < randomNumber) {
+			 
+			 System.out.println("Liczba za mala!");
+		 } else if (liczba > randomNumber) {
+			 
+			 System.out.println("Liczba za duza!");
+		 } else {
+			 
+			 System.out.println("Gratulacje!! liczba to " + randomNumber);
+			 wynik = 1;
+		 }
+		 
+		 
+		 }
+		 
+		 scanner.close();
+	}
+
+}
+
